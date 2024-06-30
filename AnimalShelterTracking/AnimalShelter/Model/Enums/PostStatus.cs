f578724566df66948bb26a17ef8374c369b3f401
Trixer <164249760+TrifunDerenj@users.noreply.github.com>
2024-06-30 18:50:41 +0200

    [ADD] Post CRUD

---
+++ AnimalShelterTracking/AnimalShelter/Model/Enums/PostStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.Model.Enums
+{
+    public enum PostStatus
+    {
+        PENDING_APPROVAL,
+        DENIED,
+        POSTED,
+        DELETED
+    }
+}
---
+++ AnimalShelterTracking/AnimalShelter/Model/Post/IPostRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.Model.Post
+{
+    public interface IPostRepo
+    {
+        int SetId();
+        void Add(Post course);
+        Post Get(int id);
+        void Update(Post course, Post updatedCourse);
+        void Delete(Post course, bool isDean);
+        List<Post> GetAll();
+        void Serialize();
+        void Deserialize();
+    }
+}

8bcf463209894a7fd9cacc37fef97452d4da2261
kuridza1 <131389372+kuridza1@users.noreply.github.com>
2024-07-02 00:15:25 +0200

    [FIX] better login/signin buttons

---
+++ AnimalShelterTracking/AnimalShelter/GUI/ViewModel/Guest/GuestWindowVM.cs
@@ -0,0 +1,39 @@
+﻿using AnimalShelter.Model.Post;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.GUI.ViewModel.Guest
+{
+    public class GuestWindowVM : INotifyPropertyChanged
+    {
+        private ObservableCollection<Post> _posts;
+        public ObservableCollection<Post> Posts
+        {
+            get => _posts;
+            set
+            {
+                _posts = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public GuestWindowVM()
+        {
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- AnimalShelterTracking/AnimalShelter/Model/Post/Post.cs
+++ AnimalShelterTracking/AnimalShelter/Model/Post/Post.cs
@@ -1,5 +1,6 @@
 ﻿using AnimalShelter.Model.Enums;
 using AnimalShelter.Model.Users;
+using AnimalShelter.Model.Pets;
 using System;
 
 namespace AnimalShelter.Model.Post
@@ -11,12 +12,13 @@ namespace AnimalShelter.Model.Post
         public Member Author { get; set; }
         public PostStatus Status { get; set; }
         public int LikesNum { get; set; }
+        public Pet Pet { get; set; }
         public Post() { }
-        public Post(int id, Member author)
+        public Post(int id, Member author, Pet pet)
         {
             Id = id;
             Author = author;
-
+            Pet = pet;
             postDate = DateTime.Now;
             Status = PostStatus.PENDING_APPROVAL;
             LikesNum = 0;

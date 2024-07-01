b056d9d81a0d7124aed7484e934c9c025b4225d0
Matijas Levang <micolevang@gmail.com>
2024-07-01 12:49:56 +0200

    [FIX] Class references

--- AnimalShelterTracking/AnimalShelter/Model/Post/Post.cs
+++ AnimalShelterTracking/AnimalShelter/Model/Post/Post.cs
@@ -1,4 +1,5 @@
 ﻿using AnimalShelter.Model.Enums;
+using AnimalShelter.Model.Users;
 using System;
 
 namespace AnimalShelter.Model.Post
@@ -7,11 +8,11 @@ namespace AnimalShelter.Model.Post
     {
         public int Id { get; set; }
         public DateTime postDate { get; set; }
-        public Member.Member Author { get; set; }
+        public Member Author { get; set; }
         public PostStatus Status { get; set; }
         public int LikesNum { get; set; }
         public Post() { }
-        public Post(int id, Member.Member author)
+        public Post(int id, Member author)
         {
             Id = id;
             Author = author;

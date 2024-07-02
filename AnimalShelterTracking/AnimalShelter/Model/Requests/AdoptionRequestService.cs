7afa1d6a23f0029f588991ab6a6d600b13595075
Matijas Levang <micolevang@gmail.com>
2024-07-02 18:59:11 +0200

    [FIX] Requests not being saved by giving them separate services

---
+++ AnimalShelterTracking/AnimalShelter/Model/Requests/AdoptionRequestService.cs
@@ -0,0 +1,47 @@
+﻿using AnimalShelter.Repository;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalShelter.Model.Requests
+{
+    public class AdoptionRequestService
+    {
+        private readonly IRepository<AdoptionRequest> _repository;
+        private const string filePath = "..\\..\\Data\\AdoptionRequests.json";
+
+        public int GetHighestIdValue()
+        {
+            return _repository.GetAll().Max(v => v.Id);
+        }
+
+        public AdoptionRequestService()
+        {
+            _repository = new JSONRepository<AdoptionRequest>(filePath);
+        }
+
+        public void Add(AdoptionRequest newAdoptionRequest)
+        {
+            _repository.Add(newAdoptionRequest);
+        }
+
+        public void Update(int requestId, AdoptionRequest newAdoptionRequest)
+        {
+            _repository.Update(requestId, newAdoptionRequest);
+        }
+
+        public AdoptionRequest Get(int requestId)
+        {
+            return _repository.Get(requestId);
+        }
+
+        public void Delete(int requestId)
+        {
+            _repository.Delete(requestId);
+        }
+
+        public List<AdoptionRequest> GetAll()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
---
+++ AnimalShelterTracking/AnimalShelter/Model/Requests/RegistrationRequestService.cs
@@ -0,0 +1,47 @@
+﻿using AnimalShelter.Repository;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalShelter.Model.Requests
+{
+    public class RegistrationRequestService
+    {
+        private readonly IRepository<RegistrationRequest> _repository;
+        private const string filePath = "..\\..\\Data\\RegistrationRequests.json";
+
+        public RegistrationRequestService()
+        {
+            _repository = new JSONRepository<RegistrationRequest>(filePath);
+        }
+
+        public int GetHighestIdValue()
+        {
+            return _repository.GetAll().Max(v => v.Id);
+        }
+
+        public void Add(RegistrationRequest newRegistrationRequest)
+        {
+            _repository.Add(newRegistrationRequest);
+        }
+
+        public void Update(int requestId, RegistrationRequest newRegistrationRequest)
+        {
+            _repository.Update(requestId, newRegistrationRequest);
+        }
+
+        public RegistrationRequest Get(int requestId)
+        {
+            return _repository.Get(requestId);
+        }
+
+        public void Delete(int requestId)
+        {
+            _repository.Delete(requestId);
+        }
+
+        public List<RegistrationRequest> GetAll()
+        {
+            return _repository.GetAll();
+        }
+    }
+}

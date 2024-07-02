41f414ec604854f314a9c13744b8b719a9d0081e
Matijas Levang <micolevang@gmail.com>
2024-07-02 15:51:38 +0200

    [ADD] Votes and votes service

---
+++ AnimalShelterTracking/AnimalShelter/Model/Votes/Vote.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimalShelter.Model.Votes
+{
+    public class Vote
+    {
+        public int Id { get; set; }
+        public DateTime TimeLimit { get; set; }
+        public int VotesFor { get; set; }
+        public int VotesAgainst { get; set; }
+        public List<int> Voters { get; set; }
+
+        public Vote() 
+        {
+            TimeLimit = DateTime.Now.AddDays(7);
+            VotesFor = 0;
+            VotesAgainst = 0;
+            Voters = new List<int>();
+        }
+
+        public Vote(int daysToVote)
+        {
+            TimeLimit = DateTime.Now.AddDays(daysToVote);
+            VotesFor = 0;
+            VotesAgainst = 0;
+            Voters = new List<int>();
+        }
+
+        public Vote(int id, DateTime timeLimit, int votesFor, int votesAgainst, List<int> voters)
+        {
+            Id = id;
+            TimeLimit = timeLimit;
+            VotesFor = votesFor;
+            VotesAgainst = votesAgainst;
+            Voters = voters;
+        }
+    }
+}
